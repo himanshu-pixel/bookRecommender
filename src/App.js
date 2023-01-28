03e2307cf2a06312b3b03d948fcec036a5c84cf0
Himanshu Kushwaha <kushwahahimanshu26@gmail.com>
2023-01-29 01:15:29 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+const books = {
+  javascript: [
+    { name: "Eloquent JS", rating: "4.5/5" },
+    { name: "You don't know JS", rating: "3.5/5" },
+  ],
+  fiction: [
+    { name: "Shiva Triology", rating: "5/5" },
+    { name: "Harry Potter and the Sorcer's stone", rating: "4.5/5" },
+  ],
+  bussiness: [
+    { name: "Forbes 30u30", rating: "1.5/5" },
+    { name: "Do Epic Shit", rating: "2.4/5" },
+  ],
+};
 
 function App() {
+  const [selectedGenre, setGenre] = useState("bussiness");
+  function genreClickHandler(genre) {
+    setGenre(genre);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Book recommender📚</h1>
+      <p>Here are the some books that we recommend</p>
+      <hr />
+      <div>
+        {Object.keys(books).map((genre) => (
+          <button onClick={() => genreClickHandler(genre)}>{genre}</button>
+        ))}
+      </div>
+      <hr />
+      <div>
+        <ul>
+          {books[selectedGenre].map((book) => (
+            <li style={{ listStyle: "none" }}>
+              <div>{book.name}</div>
+              <div>{book.rating}</div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
